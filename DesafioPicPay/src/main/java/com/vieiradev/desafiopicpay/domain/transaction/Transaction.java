2d0017adf34f9bb3970e4c07d7170fbe3bf42a91
heitorffvieira <hf.vieiraa@gmail.com>
2025-11-04 13:04:40 -0300

    feat: criando a entidade de transação

---
+++ DesafioPicPay/src/main/java/com/vieiradev/desafiopicpay/domain/transaction/Transaction.java
@@ -0,0 +1,36 @@
+package com.vieiradev.desafiopicpay.domain.transaction;
+
+import com.vieiradev.desafiopicpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
